33b7a8093902ff605bc84aa66cd09ad8e05bd354
ssabii <fhy1930@gmail.com>
2023-09-16 08:08:18 +0900

    feat: react-query App 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-import UserList from './components/UserList'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <UserList />
+      <ReactQueryDevtools />
+      <div></div>
     </QueryClientProvider>
   )
 }
