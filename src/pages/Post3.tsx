75a5f9765f5064fcdcf144587f9f3ae89bb77e50
ssabii <fhy1930@gmail.com>
2023-09-16 23:52:51 +0900

    feat: Post3 Props 수정

--- src/pages/Post3.tsx
+++ src/pages/Post3.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PostReactQuery from 'components/PostReactQuery'
 
-const Post3 = () => <PostReactQuery postId={2} />
+const Post3 = () => <PostReactQuery postId={3} />
 
 export default Post3
