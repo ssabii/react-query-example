944628b22d63a8387b8c1c296c94327e4668b35b
ssabii <fhy1930@gmail.com>
2023-09-30 23:51:13 +0900

    feat: usePostsQuery 옵션 인자 타입 수정

--- src/hooks/usePostsQuery.ts
+++ src/hooks/usePostsQuery.ts
@@ -1,8 +1,8 @@
-import { useQuery, QueryOptions } from '@tanstack/react-query'
-import { fetchPosts } from 'api/post'
+import { useQuery, UseQueryOptions } from '@tanstack/react-query'
+import { Post, fetchPosts } from 'api/post'
 
-const usePostsQuery = (options: QueryOptions = {}) => {
-  return useQuery(
+const usePostsQuery = (options?: UseQueryOptions<Post[]>) => {
+  return useQuery<Post[]>(
     ['posts'],
     fetchPosts,
     options,
