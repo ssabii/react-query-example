473153a56ddffdbcab8d4520616f302130e7c113
ssabii <fhy1930@gmail.com>
2023-10-01 00:00:35 +0900

    feat: 컴포넌트 props 수정

--- src/pages/StaleTimeFiveMinutePosts.tsx
+++ src/pages/StaleTimeFiveMinutePosts.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 import PostsReactQuery from 'components/PostsReactQuery'
 
 const StaleTimeFiveMinutePosts = () => (
-  <PostsReactQuery
-    staleTime={1000 * 60 * 5}
-    cacheTime={0}
-  />
+  <PostsReactQuery options={{
+    staleTime: 1000 * 60 * 5,
+    cacheTime: 0
+  }} />
 )
 
 export default StaleTimeFiveMinutePosts
