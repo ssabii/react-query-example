bbf8d3360a7ace902a3887d09ffc6fca1324c9b8
ssabii <fhy1930@gmail.com>
2023-10-28 01:46:32 +0900

    feat: 페이지명 일괄 수정

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,28 +1,28 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from 'components/Layout'
-import Post1 from 'pages/Post1'
-import Post2 from 'pages/Post2'
-import Post3 from 'pages/Post3'
-import ConditionPosts from 'pages/ConditionPosts'
-import StaleTimeFiveMinutePosts from 'pages/StaleTimeFiveMinutePosts'
-import QueryOneThousand from 'pages/QueryOneThousand'
-import EnableDisablePosts from 'pages/EnableDisablePosts'
-import RenderingOneThousand from 'pages/RenderingOneThousand'
-import QueryClient from 'pages/QueryClient'
+import PostOnePage from 'pages/PostOnePage'
+import PostTwoPage from 'pages/PostTwoPage'
+import PostThreePage from 'pages/PostThreePage'
+import ConditionPostsPage from 'pages/ConditionPostsPage'
+import StaleTimeFiveMinutePostsPage from 'pages/StaleTimeFiveMinutePostsPage'
+import QueryOneThousandPage from 'pages/QueryOneThousandPage'
+import EnableDisablePostsPage from 'pages/EnableDisablePostsPage'
+import RenderingOneThousandPage from 'pages/RenderingOneThousandPage'
+import QueryClientPage from 'pages/QueryClientPage'
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="post-1" element={<Post1 />} />
-        <Route path="post-2" element={<Post2 />} />
-        <Route path="post-3" element={<Post3 />} />
-        <Route path="condition-posts" element={<ConditionPosts />} />
-        <Route path="staletime-5m-posts" element={<StaleTimeFiveMinutePosts />} />
-        <Route path="query-1000" element={<QueryOneThousand />} />
-        <Route path='enable-disable-posts' element={<EnableDisablePosts />} />
-        <Route path="rendering-1000" element={<RenderingOneThousand />} />
-        <Route path="query-client" element={<QueryClient />} />
+        <Route path="post-1" element={<PostOnePage />} />
+        <Route path="post-2" element={<PostTwoPage />} />
+        <Route path="post-3" element={<PostThreePage />} />
+        <Route path="condition-posts" element={<ConditionPostsPage />} />
+        <Route path="staletime-5m-posts" element={<StaleTimeFiveMinutePostsPage />} />
+        <Route path="query-1000" element={<QueryOneThousandPage />} />
+        <Route path='enable-disable-posts' element={<EnableDisablePostsPage />} />
+        <Route path="rendering-1000" element={<RenderingOneThousandPage />} />
+        <Route path="query-client" element={<QueryClientPage />} />
       </Route>
     </Routes>
   )
